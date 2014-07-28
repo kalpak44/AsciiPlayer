d7f9c3b9f566ddefb9898362680fd7d29323163b
Ptaha <avgustares@gmail.com>
2014-07-28 15:29:29 +0300

    Fix main method error. Added readme and Demo

--- src/com/hackbulgaria/corejava/asciiplayer/Main.java
+++ src/com/hackbulgaria/corejava/asciiplayer/Main.java
@@ -2,11 +2,14 @@ package com.hackbulgaria.corejava.asciiplayer;
 
 public class Main {
 	public static void main(String[] args) {
-		String path = "img/trollface.png";
+		String path;
 		if(args.length > 0){
 			path = args[0];
+			System.out.println(new AsciiPlayer(path).getAsciiArt());
+        }else{
+        	System.out.println("Please add arguments");
         }
-	    System.out.println(new AsciiPlayer(path).getAsciiArt());
+	    
 	    
 	}
 	
